83677efe2c9b98e825bedfe2d36f554ebec29877
xiaoyu <1017798646@qq.com>
2021-07-26 22:31:42 +0800

    添加工具类

---
+++ src/main/java/com/xiaoyu/utils/ConstantUtil.java
@@ -0,0 +1,11 @@
+package com.xiaoyu.utils;
+
+/**
+ * 常量工具类
+ */
+public class ConstantUtil {
+    // 小写字母数组
+    public static final char[] letter_lowercase_array = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+    // 大写字母数组
+    public static final char[] letter_uppercase_array = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+}
---
+++ src/main/java/com/xiaoyu/utils/StringUtil.java
@@ -0,0 +1,9 @@
+package com.xiaoyu.utils;
+
+/**
+ * 字符串工具类
+ */
+public class StringUtil {
+
+
+}
