db68ebf92f38310bf73886ec67042e02445de3b2
gqshuang <ganqinshuang@qq.com>
2021-09-08 17:21:24 +0800

    新增http请求工具类，基于Apache HttpClient

--- src/main/java/com/xiaoyu/utils/DateUtil.java
+++ src/main/java/com/xiaoyu/utils/DateUtil.java
@@ -1,6 +1,6 @@
 package com.xiaoyu.utils;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 
 import java.text.DateFormat;
 import java.text.ParseException;
