bd1b5d35799fd7484d29911da2c1e09b83eae8bc
gqshuang <ganqinshuang@qq.com>
2021-09-10 17:19:21 +0800

    添加笔记，脚本

--- src/main/resources/script/xiaoyu_website.sql
+++ src/main/resources/script/xiaoyu_website.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50732
  File Encoding         : 65001
 
- Date: 09/09/2021 18:02:28
+ Date: 10/09/2021 17:18:00
 */
 
 SET NAMES utf8mb4;
@@ -150,7 +150,7 @@ CREATE TABLE `income`  (
   `createBy` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '创建人',
   `remark` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '备注',
   PRIMARY KEY (`incomeId`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 22 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '收入表' ROW_FORMAT = DYNAMIC;
+) ENGINE = InnoDB AUTO_INCREMENT = 23 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '收入表' ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of income
@@ -160,6 +160,7 @@ INSERT INTO `income` VALUES (18, 8, '截至2021-09-07支付宝账户余额', 116
 INSERT INTO `income` VALUES (19, 8, '截至2021-09-07交通银行余额', 2141.51, '2021-09-07 13:00:00', '银行：交通银行\n明细：账户余额', '2021-09-07 13:04:44', NULL, NULL);
 INSERT INTO `income` VALUES (20, 8, '截至2021-09-07工商银行余额', 8.43, '2021-09-07 13:00:00', '银行：工商银行\n明细：账户余额', '2021-09-07 13:06:13', NULL, NULL);
 INSERT INTO `income` VALUES (21, 9, '2021-08-30至2021-09-07累计消费', 2094.45, '2021-09-07 13:00:00', '累计消费', '2021-09-07 13:08:43', NULL, NULL);
+INSERT INTO `income` VALUES (22, 7, '8月份工资', 7426.55, '2021-09-10 09:20:00', '银行：苏州银行\n明细：9月10号发放的8月份工资', '2021-09-10 09:22:39', NULL, NULL);
 
 -- ----------------------------
 -- Table structure for income_cat
@@ -216,19 +217,23 @@ CREATE TABLE `note`  (
   `createDt` datetime NULL DEFAULT NULL COMMENT '创建时间',
   `createBy` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '创建人',
   PRIMARY KEY (`noteId`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 9 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '笔记表' ROW_FORMAT = DYNAMIC;
+) ENGINE = InnoDB AUTO_INCREMENT = 13 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '笔记表' ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of note
 -- ----------------------------
-INSERT INTO `note` VALUES (1, 'apache-poi 直接解析本地Excel', 1, NULL, '// Java 读写 excel\nhttps://zhuanlan.zhihu.com/p/74922498\n\n// 省市对照表 2020版\nhttps://www.b910.cn/tool/1.htm', 0, 0, '2021-09-06 10:28:48', NULL);
-INSERT INTO `note` VALUES (2, 'Maven安装三方Jar包到本地仓库', 1, NULL, '// 需要配置Maven本地环境变量\nmvn install:install-file \n    -DgroupId=Msc // groupId\n    -DartifactId=Msc // artifactId\n    -Dversion=1.0 // version\n    -Dpackaging=jar // jar\n    -Dfile=Msc.jar  // 文件名\n\n// 示例, 首先定位到 Msc.jar 所在目录, 再执行下面的命令\nmvn install:install-file -DgroupId=Msc -DartifactId=Msc -Dversion=1.0 -Dpackaging=jar -Dfile=Msc.jar ', 0, 0, '2021-09-06 10:32:11', NULL);
+INSERT INTO `note` VALUES (1, 'Apache-poi 直接解析本地Excel', 1, NULL, '\n## 一、相关资源\n<a href=\"https://zhuanlan.zhihu.com/p/74922498\" target=\"_blank\">Java 读写 excel</a>\n<a href=\"https://www.b910.cn/tool/1.htm\" target=\"_blank\">省市对照表 2020版</a>\n\n## 二、代码\n1.`pom.xml`\n\n```xml\n<dependency>\n<groupId>org.apache.poi</groupId>\n<artifactId>poi</artifactId>\n<version>4.1.2</version>\n</dependency>\n\n<dependency>\n<groupId>org.apache.poi</groupId>\n<artifactId>poi-ooxml</artifactId>\n<version>4.1.2</version>\n</dependency>\n```\n\n2.`java`代码\n\n```java\npublic void parseProvAndCity() {\n    File city = new File(\"D:\\\\city.xlsx\");\n\n    try {\n        // 构建Excel工作簿\n        Workbook workbook = WorkbookFactory.create(city);\n        // 获取Excel工作表数量\n        int sheets = workbook.getNumberOfSheets();\n        if (sheets < 1) return; // 表示为空\n        // 获取第一张表, 从0开始\n        Sheet sheet = workbook.getSheetAt(0);\n\n        // 用于存储对象\n        List<SysArea> list = new ArrayList<>();\n        // 获取有数据的行数\n        int rowNumbers = sheet.getLastRowNum();\n        // 遍历开始解析\n        for (int i = 1; i < rowNumbers; i++) {\n            Row row = sheet.getRow(i); // 行\n            // 列,第一列从0开始; 此处获取身份证上的编号\n            String areaNum = String.valueOf(Integer.parseInt(row.getCell(0).toString()));\n            // 排除市以下的区县\n            if (!\"00\".equals(areaNum.substring(4, 6))) continue;\n            // 对应省市\n            String areaName = row.getCell(1).toString();\n            String[] split = areaName.split(\",\");\n            areaName = split[split.length - 1];\n            // 封装实体\n            SysArea sysArea = new SysArea();\n            sysArea.setAreaNum(areaNum);\n            sysArea.setAreaName(areaName);\n            list.add(sysArea);\n        }\n        // 遍历存到数据库\n        list.forEach((sysArea -> jobFairMapper.insertSysArea(sysArea)));\n    } catch (IOException e) {\n        e.printStackTrace();\n    }\n}\n```', 0, 0, '2021-09-06 10:28:48', NULL);
+INSERT INTO `note` VALUES (2, 'Maven安装三方Jar包到本地仓库', 1, NULL, '```shell\n// 需要配置Maven本地环境变量\nmvn install:install-file \n    -DgroupId=Msc // groupId\n    -DartifactId=Msc // artifactId\n    -Dversion=1.0 // version\n    -Dpackaging=jar // jar\n    -Dfile=Msc.jar  // 文件名\n\n// 示例, 首先定位到 Msc.jar 所在目录, 再执行下面的命令\nmvn install:install-file -DgroupId=Msc -DartifactId=Msc -Dversion=1.0 -Dpackaging=jar -Dfile=Msc.jar\n```', 0, 0, '2021-09-06 10:32:11', NULL);
 INSERT INTO `note` VALUES (3, '访客机硬件接口对接', 2, NULL, '标题：对接大数据访客信息', 0, 0, '2021-09-06 10:33:52', NULL);
 INSERT INTO `note` VALUES (4, '既许一人以偏爱，愿尽余生之慷慨', 7, NULL, '既许一人以偏爱，愿尽余生之慷慨【爱情】', 0, 0, '2021-09-06 11:00:25', NULL);
 INSERT INTO `note` VALUES (5, 'Git提交报错', 8, NULL, '报错：OpenSSL SSL_read: Connection was reset, errno 10054\n解决：https://blog.csdn.net/weixin_43945983/article/details/110882074', 0, 0, '2021-09-06 11:13:18', NULL);
 INSERT INTO `note` VALUES (6, 'Post请求之 java.net', 1, NULL, '    /**\n     * 向指定 URL 发送POST方法的请求\n     *\n     * @param url   发送请求的 URL\n     * @param param 请求参数，请求参数应该是 name1=value1&name2=value2 的形式。\n     * @return 所代表远程资源的响应结果\n     */\n    public static String sendPost(String url, JSONObject param) {\n        PrintWriter out = null;\n        BufferedReader in = null;\n        String result = \"\";\n        try {\n            URL realUrl = new URL(url);\n            // 打开和URL之间的连接\n            URLConnection conn = realUrl.openConnection();\n            // 设置通用的请求属性\n            conn.setRequestProperty(\"accept\", \"*/*\");\n            conn.setRequestProperty(\"connection\", \"Keep-Alive\");\n            conn.setRequestProperty(\"user-agent\", \"Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)\");\n            // 发送POST请求必须设置如下两行\n            conn.setDoOutput(true);\n            conn.setDoInput(true);\n            // 获取URLConnection对象对应的输出流\n            out = new PrintWriter(conn.getOutputStream());\n            // 发送请求参数\n            out.print(param);\n            // flush输出流的缓冲\n            out.flush();\n            // 定义BufferedReader输入流来读取URL的响应\n            in = new BufferedReader(new InputStreamReader(conn.getInputStream()));\n            String line;\n            while ((line = in.readLine()) != null) {\n                result += line;\n            }\n        } catch (Exception e) {\n            System.out.println(\"发送 POST 请求出现异常！\" + e);\n            e.printStackTrace();\n        }\n        // 使用finally块来关闭输出流、输入流\n        finally {\n            try {\n                if (out != null) {\n                    out.close();\n\n                }\n                if (in != null) {\n                    in.close();\n                }\n            } catch (IOException ex) {\n                ex.printStackTrace();\n            }\n        }\n        return result;\n    }', 0, 0, '2021-09-08 11:00:39', NULL);
 INSERT INTO `note` VALUES (7, 'Post请求之 Apache', 1, NULL, '/**\n     * 发送post请求\n     *\n     * @param url      路径\n     * @param params   参数(json类型)\n     * @param encoding 编码格式\n     * @return 响应报文\n     */\n    public static String sendPost(String url, JSONObject params, String encoding) {\n        String body = \"\";\n\n        //创建httpclient对象\n        CloseableHttpClient client = HttpClients.createDefault();\n        //创建post方式请求对象\n        HttpPost httpPost = new HttpPost(url);\n\n        //装填参数\n        StringEntity s = new StringEntity(params.toString(), \"utf-8\");\n        s.setContentEncoding(new BasicHeader(HTTP.CONTENT_TYPE, \"application/json\"));\n        //设置参数到请求对象中\n        httpPost.setEntity(s);\n        // System.out.println(\"请求地址：\" + url);\n        // System.out.println(\"请求参数：\"+ nvps.toString());\n\n        // 设置header信息\n        // 指定报文头【Content-type】、【User-Agent】\n        // httpPost.setHeader(\"Content-type\", \"application/x-www-form-urlencoded\");\n        httpPost.setHeader(\"Content-type\", \"application/json\");\n        httpPost.setHeader(\"User-Agent\", \"Mozilla/4.0 (compatible; MSIE 5.0; Windows NT; DigExt)\");\n\n        //执行请求操作，并拿到结果（同步阻塞）\n        CloseableHttpResponse response = null;\n        try {\n            response = client.execute(httpPost);\n            //获取结果实体\n            HttpEntity entity = response.getEntity();\n            if (entity != null) {\n                //按指定编码转换结果实体为String类型\n                body = EntityUtils.toString(entity, encoding);\n            }\n            EntityUtils.consume(entity);\n        } catch (IOException e) {\n            e.printStackTrace();\n        } finally {\n            //释放链接\n            try {\n                response.close();\n            } catch (IOException e) {\n                e.printStackTrace();\n            }\n        }\n        return body;\n    }', 0, 0, '2021-09-08 11:02:18', NULL);
 INSERT INTO `note` VALUES (8, 'HahsMap源码', 5, NULL, NULL, 0, 0, '2021-09-09 13:50:46', NULL);
+INSERT INTO `note` VALUES (9, '日期格式化', 5, NULL, '```javascript\n// 方法\nfunction dateFormat(fmt, date) {\n    let ret;\n    const opt = {\n        \"Y+\": date.getFullYear().toString(),        // 年\n        \"m+\": (date.getMonth() + 1).toString(),     // 月\n        \"d+\": date.getDate().toString(),            // 日\n        \"H+\": date.getHours().toString(),           // 时\n        \"M+\": date.getMinutes().toString(),         // 分\n        \"S+\": date.getSeconds().toString()          // 秒\n        // 有其他格式化字符需求可以继续添加，必须转化成字符串\n    };\n    for (let k in opt) {\n        ret = new RegExp(\"(\" + k + \")\").exec(fmt);\n        if (ret) {\n            fmt = fmt.replace(ret[1], (ret[1].length == 1) ? (opt[k]) : (opt[k].padStart(ret[1].length, \"0\")))\n        }\n    }\n    return fmt;\n}\n\n// 调用 YYYY-mm-dd HH:MM:SS\nconsole.log(dateFormat(\"YYYY-mm-dd 23:59:59\", new Date()));\n```', 0, 0, '2021-09-09 19:12:37', NULL);
+INSERT INTO `note` VALUES (10, '自定义搜索下拉', 5, NULL, '```html\n// 标签, 这里使用了 Vue.js\n<div class=\"col-sm-3\" id=\"contextCompanyBox\">\n    <input name=\"companyName\" type=\"text\" value=\"\" class=\"form-control form-control-sm\" placeholder=\"企业名称\" data-content=\"企业名称\" autocomplete=\"off\"\n           v-model:value=\"configBean.companyName\" @input.stop=\"jobfair.companyNameChanged();\" @click=\"jobfair.companyNameCLick();\"/>\n    <ul class=\"dropdown-menu shadow-sm tal-dropdown-scroll\" @hover=\"jobfair.companyNameBlur();\"\n        style=\"margin-left: 14px; width: 90%; height: auto; max-height: 200px;\"\n        aria-labelledby=\"dropdownMenu\" id=\"companyDropdown\">\n        <li class=\"tal-dropdown-item\" v-for=\"company in configBean.companyDropList\" @click=\"jobfair.selectAddCompay(company.id,company.companyName);\">\n            {{company.companyName}}\n        </li>\n    </ul>\n</div>\n\n```\n```css\n// 样式\n.tal-dropdown-scroll {\n    display: block;\n    z-index: 999;\n    background: #fff;\n    border: 1px solid #ccc;\n    padding: 2px;\n    width: 180px;\n    height: 100px;\n    overflow-x: hidden;\n    overflow-y: scroll;\n}\n```\n```javascript\n// 监听，除了点击自己，点击其他地方将自身隐藏\ndocument.addEventListener(\"click\", e => {\n    const contextCompanyBox = document.getElementById(\"contextCompanyBox\");\n    if (contextCompanyBox) {\n        if (!contextCompanyBox.contains(e.target)) {\n            this.rightFlag = false;\n            $(\"#companyDropdown\").hide();\n        }\n    }\n});\n```', 0, 0, '2021-09-09 19:17:08', NULL);
+INSERT INTO `note` VALUES (11, 'LeetCode题解，001：两数之和', 10, NULL, '## 两数之和\n\n**题目描述：**\n1. 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那两个整数，并返回它们的数组下标。\n2. 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。\n3. 你可以按任意顺序返回答案。\n\n**示例 1：**\n> 输入：nums = [2,7,11,15], target = 9\n> 输出：[0,1]\n> 解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。\n\n**示例 2：**\n> 输入：nums = [3,2,4], target = 6\n> 输出：[1,2]\n> 示例 3：\n> 输入：nums = [3,3], target = 6\n> 输出：[0,1]\n\n**提示：**\n> 2 <= nums.length <= 104\n> -109 <= nums[i] <= 109\n> -109 <= target <= 109\n> 只会存在一个有效答案\n\n**进阶：你可以想出一个时间复杂度小于 O(n2) 的算法吗？**\n> 来源：力扣（LeetCode）\n> 链接：https://leetcode-cn.com/problems/two-sum\n\n**```java``` 题解:**\n\n```java\npublic int[] twoSum(int[] nums, int target) {\n	if (nums != null && nums.length > 0) {\n		for (int i = 0; i < nums.length; i++) {\n			for (int j = i + 1; j < nums.length; j++) {\n				if (nums[i] + nums[j] == target) {\n					return new int[]{i, j};\n				}\n			}\n		}\n	}\n	throw new IllegalArgumentException(\"No two sum solution\");\n}\n```\n', 0, 0, '2021-09-10 13:13:13', NULL);
+INSERT INTO `note` VALUES (12, '公司项目访问地址', 9, NULL, '### 一、办公\n**1. 考勤地址**\n```\nhttp://112.4.75.18/cpcm\n```\n\n**2. 公司邮箱**\n```\nhttp://mail.gmwinlead.com/\n```\n\n### 二、项目\n**1. 复旦项目**\n```\n测试地址：http://192.168.16.158:8080/datacenter/\n远程桌面：\n	IP地址: 192.168.16.158\n	登录名称：Administrator\n	登录密码：guomao-12345\n```\n\n**2. 停车收费系统**\n```\n地址：http://192.168.18.187:8080/parkweb\n```\n\n**3. 苏停车**\n```\n地址：http://192.168.18.226:8087/elifeweb/\n```\n\n**4. 市政府2期项目**\n```\n公网地址：http://yunweigmwinlead.cn/access\n测试地址：http://192.168.18.214:8080/access\n远程桌面：\n	IP地址：192.168.18.214\n	登录名称：Administrator\n	登录密码：guomao-12345\n```\n\n**5. 运维项目**\n```\n测试地址 http://192.168.16.158:8080/dcomw/\n```\n\n------------\n', 0, 0, '2021-09-10 16:35:00', NULL);
 
 -- ----------------------------
 -- Table structure for note_cat
@@ -244,7 +249,7 @@ CREATE TABLE `note_cat`  (
   `createBy` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '创建人',
   `parentNoteCatNum` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '父级编号',
   PRIMARY KEY (`noteCatId`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 10 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '笔记分类表' ROW_FORMAT = DYNAMIC;
+) ENGINE = InnoDB AUTO_INCREMENT = 11 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '笔记分类表' ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of note_cat
@@ -258,6 +263,7 @@ INSERT INTO `note_cat` VALUES (6, '杂事百科', '0005', 0, 0, '2021-09-06 09:1
 INSERT INTO `note_cat` VALUES (7, '句子笔记', '0006', 0, 0, '2021-09-06 10:59:55', '', NULL);
 INSERT INTO `note_cat` VALUES (8, 'Bug笔记', '0007', 0, 4, '2021-09-06 11:12:55', '', NULL);
 INSERT INTO `note_cat` VALUES (9, '国贸酝领', '00020001', 2, 0, '2021-09-09 14:06:32', '', '0002');
+INSERT INTO `note_cat` VALUES (10, '算法笔记', '00010002', 1, 0, '2021-09-10 13:07:37', '', '0001');
 
 -- ----------------------------
 -- Table structure for plan
@@ -354,7 +360,7 @@ CREATE TABLE `spend`  (
   `createBy` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '创建人',
   `remark` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '备注',
   PRIMARY KEY (`spendId`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 20 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '消费表' ROW_FORMAT = DYNAMIC;
+) ENGINE = InnoDB AUTO_INCREMENT = 21 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '消费表' ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of spend
@@ -378,6 +384,7 @@ INSERT INTO `spend` VALUES (16, 6, '迅雷会员', 8.90, '2021-09-05 20:00:00', 
 INSERT INTO `spend` VALUES (17, 1, '沙拉鸡排饭+疯狂龙脆骨', 20.59, '2021-09-08 19:00:00', '平台：饿了吗\n商家：嘿米饭堂(跨塘店)', '2021-09-09 08:49:06', NULL, NULL);
 INSERT INTO `spend` VALUES (18, 2, '文明Ⅵ+DLC', 50.40, '2021-09-04 20:00:00', '平台：Steam\n', '2021-09-09 08:51:50', NULL, NULL);
 INSERT INTO `spend` VALUES (19, 1, '饿了吗-超级吃货卡【会员】', 0.90, '2021-09-05 11:00:00', '平台：饿了吗\n', '2021-09-09 08:56:48', NULL, NULL);
+INSERT INTO `spend` VALUES (20, 4, '京东购物', 561.80, '2021-09-10 12:00:00', '平台：京东商城\n明细：【硬盘】+【...】', '2021-09-10 12:18:37', NULL, NULL);
 
 -- ----------------------------
 -- Table structure for spend_cat
