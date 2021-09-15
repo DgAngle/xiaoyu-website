e926d3c80af9fb6c8187371e0715dbb4d22f0e53
xiaoyu <1017798646@qq.com>
2021-09-15 21:38:04 +0800

    阿里云Oss存储

--- src/main/java/com/xiaoyu/utils/ConstantUtil.java
+++ src/main/java/com/xiaoyu/utils/ConstantUtil.java
@@ -1,12 +1,48 @@
 package com.xiaoyu.utils;
 
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Value;
+
 import java.util.HashMap;
 import java.util.Map;
 
 /**
  * 常量工具类
  */
-public class ConstantUtil {
+public class ConstantUtil implements InitializingBean {
+
+    // 读取配置文件内容
+    @Value("${aliyun.oss.file.endpoint}")
+    private String endpoint;
+
+    @Value("${aliyun.oss.file.keyid}")
+    private String keyId;
+
+    @Value("${aliyun.oss.file.keysecret}")
+    private String keySecret;
+
+    @Value("${aliyun.oss.file.bucketname}")
+    private String bucketName;
+
+    // 定义公开的静态常量
+    public static String END_POINT;
+    public static String ACCESS_KEY_ID;
+    public static String ACCESS_KEY_SECRET;
+    public static String BUCKET_NAME;
+
+    /**
+     * 当值初始化后执行该方法
+     *
+     * @throws Exception
+     */
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        END_POINT = this.endpoint;
+        ACCESS_KEY_ID = this.keyId;
+        ACCESS_KEY_SECRET = this.keySecret;
+        BUCKET_NAME = this.bucketName;
+    }
+
     // 小写字母数组
     public static final char[] letter_lowercase_array = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
     // 大写字母数组
@@ -35,6 +71,7 @@ public class ConstantUtil {
     public static final String plan_status_unfinished_message = "未完成";
     /* 计划 Map */
     public static final Map<Integer, String> planMap;
+
     static {
         planMap = new HashMap<Integer, String>();
         planMap.put(plan_status_not_start, plan_status_not_start_message);
@@ -44,5 +81,7 @@ public class ConstantUtil {
         // planMap.put(plan_status_delay_finished, plan_status_delay_finished_message);
         planMap.put(plan_status_finished, plan_status_finished_message);
         planMap.put(plan_status_unfinished, plan_status_unfinished_message);
-    };
+    }
+
+    ;
 }
