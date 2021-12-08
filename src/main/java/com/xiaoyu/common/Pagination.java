2cfbf2e30a7a4342887a3adc9733b5f4565f45c0
gqshuang <ganqinshuang@qq.com>
2021-12-08 11:03:08 +0800

    列表高度调整, 添加日历、笔记标签页标题文字

--- src/main/java/com/xiaoyu/common/Pagination.java
+++ src/main/java/com/xiaoyu/common/Pagination.java
@@ -8,7 +8,7 @@ import lombok.Data;
 @Data
 public class Pagination {
     private int start; // 开始数目
-    private int pageCount = 15; // 每页数量
+    private int pageCount = 12; // 每页数量
     private int totalCount; // 总数量
     private int currentPage = 1; // 当前页码
     private int totalPage; // 总页码
